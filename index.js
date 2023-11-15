46333622a5ccc1399b14cc9e7f9911d8c1a62262
Mahadi1000 <muhammadmahdihasan1@gmail.com>
2023-11-15 19:34:18 +0600

    successfully update data

--- index.js
+++ index.js
@@ -46,27 +46,95 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/bookings", async (req, res) => {
-      const book = allBookingCollection.find();
-      const result = await book.toArray();
-      console.log(result);
-      res.send(result);
-    });
+app.get("/allServices/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }; // Convert id to ObjectId
+  const result = await allServiceCollection.findOne(query);
+  res.send(result);
+});
 
-    app.get("/bookings/:id", async (req, res) => {
-      console.log(req.params);
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await allBookingCollection.findOne(query);
-      res.send(result);
-      console.log(result);
-    });
+app.delete("/allServices/:id", async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const id = req.params.id;
 
-    app.post("/bookings", async (req, res) => {
-      const booking = req.body;
-      const result = await allBookingCollection.insertOne(booking);
-      res.send(result);
-    });
+    const filter = { _id: id }; // Assuming id is a string
+
+    const result = await allBookingCollection.deleteOne(filter);
+
+    if (result.deletedCount > 0) {
+      console.log("Document deleted successfully.");
+      res.status(200).json({ deletedCount: result.deletedCount });
+    } else {
+      console.log("No matching document found for deletion.");
+      res
+        .status(404)
+        .json({ error: "No matching document found for deletion." });
+    }
+  } catch (error) {
+    console.error("Error deleting booking:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.put("/allServices/:id", async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const options = { upsert: true };
+  const updateService = req.body;
+  const service = {
+    $set: {
+      name: updateService.name,
+      services: updateService.services,
+      type: updateService.type,
+      price: updateService.price,
+      description: updateService.description,
+      providerImage: updateService.providerImage,
+      serviceArea: updateService.serviceArea,
+      photo: updateService.photo,
+    },
+  };
+
+  try {
+    const result = await allServiceCollection.updateOne(
+      filter,
+      service,
+      options
+    );
+
+    if (result.matchedCount > 0) {
+      res.status(200).json({ modifiedCount: result.modifiedCount });
+    } else {
+      res.status(404).json({ error: "No matching document found for update." });
+    }
+  } catch (error) {
+    console.error("Error updating service:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+app.get("/bookings", async (req, res) => {
+  const book = allBookingCollection.find();
+  const result = await book.toArray();
+  console.log(result);
+  res.send(result);
+});
+
+app.get("/bookings/:id", async (req, res) => {
+  console.log(req.params);
+  const id = req.params.id;
+  const query = { _id: id };
+  const result = await allBookingCollection.findOne(query);
+  res.send(result);
+  console.log(result);
+});
+
+app.post("/bookings", async (req, res) => {
+  const booking = req.body;
+  const result = await allBookingCollection.insertOne(booking);
+  res.send(result);
+});
 app.delete("/bookings/:id", async (req, res) => {
   try {
     console.log(req.params.id);
